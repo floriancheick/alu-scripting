55736f92ff575a1f0e80712ca212b3f8d078ba55
root <f.nganji@alustudent.com>
2022-10-21 20:52:18 +0000

    2-recurse.py

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """script"""
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    param = {'after': after}
+    res = requests.get(url, headers=header, params=param)
+
+    if res.status_code != 200:
+        return None
+    else:
+        json_res = res.json()
+
+        after = json_res.get('data').get('after')
+        has_next = \
+            json_res.get('data').get('after') is not None
+
+        hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title'))
+         for article in hot_articles]
+
+        return recurse(subreddit, hot_list, after=after) \
+            if has_next else hot_list
